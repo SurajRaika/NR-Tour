2da996a60f95825e31fe45a1cc5a620f59dfa057
suraj raika <surajraika5sr@gmail.com>
2025-03-27 22:41:57 +0530

    Add new content modules, update favicons, and modify service page redirects

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,8 @@
+
+export default new Map([
+["src/content/trips/Pushkar.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Ftrips%2FPushkar.mdx&astroContentModuleFlag=true")],
+["src/content/trips/udaipur.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Ftrips%2Fudaipur.mdx&astroContentModuleFlag=true")],
+["src/content/trips/udaipur copy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Ftrips%2Fudaipur+copy.mdx&astroContentModuleFlag=true")],
+["src/content/trips/jodhpur.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Ftrips%2Fjodhpur.mdx&astroContentModuleFlag=true")],
+["src/content/trips/jaipur.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Ftrips%2Fjaipur.mdx&astroContentModuleFlag=true")]]);
+		
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,36 @@
 import { defineCollection, z } from "astro:content";
 
 const postsCollection = defineCollection({
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string()
+    }),
+    tags: z.array(z.string())
+  })
+});
 
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-
-      tags: z.array(z.string())
-    })
- });
+const tripsCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    location: z.string(),
+    realCost: z.number(),
+    discountedCost: z.number(),
+    rating: z.number(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string()
+    }),
+    tags: z.array(z.string())
+  })
+});
 
 export const collections = {
   posts: postsCollection,
+  trips: tripsCollection
 };
